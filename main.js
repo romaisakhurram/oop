a10fb164ebb9e0f5325f538ee96e60f885592c02
*romaisa <*mahamkhurram1230@gmail.com>
2024-06-24 21:49:17 +0500

    create oop game

---
+++ main.js
@@ -0,0 +1,57 @@
+import inquirer from "inquirer";
+class Student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class Person {
+    students = [];
+    addStudent(obj) {
+        this.students.push(obj);
+    }
+}
+const persons = new Person();
+const startProgram = async (persons) => {
+    do {
+        console.log("Welcome");
+        const ans = await inquirer.prompt([
+            {
+                name: "select",
+                type: "list",
+                message: "Whom would yuo like to interact with?",
+                choices: ["Staff", "Student", "Exit"]
+            }
+        ]);
+        if (ans.select == "Staff") {
+            console.log("You will aproach the staff room. Please feel free to ask any question");
+        }
+        else if (ans.select == "Student") {
+            const ans = await inquirer.prompt([
+                {
+                    name: "student",
+                    type: "input",
+                    message: "Enter the Student's name you wish to engage with:"
+                }
+            ]);
+            const student = persons.students.find(val => val.name === ans.student);
+            if (!student) {
+                const name = new Student(ans.student);
+                persons.addStudent(name);
+                console.log(`Hello i am ${name.name}, Nice to meet you.`);
+                console.log("New Student Added");
+                console.log("Current Student List :");
+                console.log(persons.students);
+            }
+            else {
+                console.log(`Hello ${Student.name}, Nice to see you again`);
+                console.log("Existing Student List");
+                console.log(persons.students);
+            }
+        }
+        else if (ans.select == "Exit") {
+            console.log("Existing the program ...");
+        }
+    } while (true);
+};
+startProgram(persons);
